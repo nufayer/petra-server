242396d57e34e5b8df8275d92a669c712b7caea0
nufayer <nufayer6543@gmail.com>
2026-05-19 09:30:31 +0600

    connected form to database

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 dotenv.config();
 
@@ -8,6 +9,9 @@ const uri = process.env.MONGODB_URI;
 
 const app = express();
 const PORT = process.env.PORT
+
+app.use(cors());
+app.use(express.json());
 
 
 const client = new MongoClient(uri, {
@@ -20,14 +24,23 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    // Send a ping to confirm a successful connection
+
+    const db = client.db("petra");
+    const petCollection = db.collection("pets");
+
+    app.post('/pet', async (req, res) => {
+        const petData = req.body;
+        console.log(petData);
+        const result = await petCollection.insertOne(petData);
+        res.send(result);
+    });
+
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
+  
+    // await client.close();
   }
 }
 run().catch(console.dir);
